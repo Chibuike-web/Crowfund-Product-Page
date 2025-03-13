148f88d10749ab9480585fad15a07271d234491f
Maduabuchi Chibuike <juniortechbro@gmail.com>
2025-03-14 00:38:40 +0100

    implemented the hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,50 @@
 import React from "react";
 import Navbar from "./Navbar";
 import styles from "./CrowdfundPage.module.css";
+import Button from "./Button";
+import { BookmarkIcon } from "./Icons";
 
 export default function Hero() {
 	return (
-		<main className={styles.hero}>
-			<Navbar />
+		<main className="flex flex-col items-center">
+			<section className={styles.hero}>
+				<Navbar />
+			</section>
+			<ProductSection />
 		</main>
 	);
 }
+
+const ProductSection = () => {
+	return (
+		<section className="relative mt-[-5.8125rem] bg-white w-full max-w-[45.625rem] border border-gray-200 rounded-[0.5rem] px-12">
+			<figure className="absolute left-[50%] -translate-y-1/2 -translate-x-1/2">
+				<img src="/logo-mastercraft.svg" alt="Logo Mastercraft" />
+			</figure>
+			<h1 className="mt-[3.5625rem] text-center font-bold text-[1.75rem] text-black">
+				Mastercraft Bamboo Monitor Riser
+			</h1>
+			<p className="text-center text-dark-gray font-medium mt-4">
+				A beautiful & handcrafted monitor stand to reduce neck and eye strain.
+			</p>
+			<div className="mt-10 mb-10 flex justify-between">
+				<Button
+					text="Back this project"
+					className="w-[12.75rem] h-[3.5rem] bg-moderate-cyan text-white rounded-full font-bold"
+				/>
+				<BookmarkButton />
+			</div>
+		</section>
+	);
+};
+
+const BookmarkButton = () => {
+	return (
+		<button
+			type="button"
+			className="flex gap-[10px] items-center font-bold text-dark-gray w-[176px] h-[3.5rem] bg-[rgba(122,122,122,0.1)] rounded-full"
+		>
+			<BookmarkIcon /> Bookmark
+		</button>
+	);
+};
--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -50,3 +50,27 @@ export const Cancel = ({ className = "" }) => {
 		</svg>
 	);
 };
+
+export const BookmarkIcon = ({ className = "" }) => {
+	return (
+		<svg width="56" height="56" viewBox="0 0 56 56" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<g clipPath="url(#clip0_2063_253)">
+				<path
+					d="M28 56C43.464 56 56 43.464 56 28C56 12.536 43.464 0 28 0C12.536 0 0 12.536 0 28C0 43.464 12.536 56 28 56Z"
+					fill="#2F2F2F"
+				/>
+				<path
+					fillRule="evenodd"
+					clipRule="evenodd"
+					d="M23 19V37L28 31.942L33 37V19H23Z"
+					fill="#B1B1B1"
+				/>
+			</g>
+			<defs>
+				<clipPath id="clip0_2063_253">
+					<rect width="56" height="56" fill="white" />
+				</clipPath>
+			</defs>
+		</svg>
+	);
+};
